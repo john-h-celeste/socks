7ffd72ac51059e7aa5663d2c15692536f520b8df
john-h-celeste-burger <rvail8330@floridapoly.edu>
2025-10-21 15:13:17 -0400

    change the message format

--- message.py
+++ message.py
@@ -1,12 +1,36 @@
 import config
 
+statuses = [
+    'OK', 'ERR',
+    'AUTH', 'CRED', # authentication
+    'UPLOAD', 'DOWNLOAD', 'MKDIR', 'RM', 'DIR', # filesystem operations
+    'DATA', 'ACK', # file data
+]
+
+def statustocode(s):
+    return statuses.index(s)
+
+def codetostatus(c):
+    return statuses[c]
+
+def sendbin(conn, status, message):
+    assert status in statuses, f'unrecognized status: {status}'
+    conn.send(struct.pack('<BI', statustocode(status), len(message)) + message)
+
+def recvbin(conn):
+    data = ''
+    while True:
+        data += conn.recv(config.chunksize)
+        if len(data) >= struct.calcsize('<BI'):
+            code,datalen = struct.unpack_from('<BI', data)
+            if len(data) >= datalen + struct.calcsize('<BI'):
+                break
+    code,datalen = struct.unpack_from('<BI', data)
+    return codetostatus(code), data[struct.calcsize('<BI'):struct.calcsize('<BI') + datalen]
+
 def send(conn, status, message):
-    assert '@' not in status, 'the status may not contain an at sign (\'@\')'
-    assert '\0' not in message, 'the message may not contain a null character (\'\\0\')'
-    conn.send((status + '@' + message + '\0').encode(config.encoding))
+    sendbin(conn, status, message.encode(config.encoding)
 
 def recv(conn):
-    data = ''
-    while not data.endswith('\0'):
-        data += conn.recv(config.chunksize).decode(config.encoding)
-    return data[:-1].split('@', maxsplit = 1)
+    status,data = recvbin(conn)
+    return status, data.decode(config.encoding)
